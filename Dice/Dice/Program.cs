9c91201d689d23c01135879a655276260423ef7c
Mustafa Slama <76624245+mustafaslamv@users.noreply.github.com>
2024-01-18 00:42:27 +0200

    add Dice game

---
+++ Dice/Dice/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Dice
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int playerRandNum;
+            int enemyRandNum;
+            int playerScore = 0;
+            int enemyScore = 0;
+            ConsoleKeyInfo key;
+
+            Random random = new Random();
+            while (true)
+            {
+                Console.WriteLine("Press Enter to roll the dice, 'Q' to exit");
+                key = Console.ReadKey();
+
+                if (key.Key != ConsoleKey.Q)
+                {
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        playerRandNum = random.Next(1, 7);
+                        enemyRandNum = random.Next(1, 7);
+
+                        Thread.Sleep(300);
+                        Console.WriteLine("\nYou rolled: {0}", playerRandNum);
+                        Thread.Sleep(300);
+                        Console.WriteLine("Enemy rolled: {0}", enemyRandNum);
+                        if (playerRandNum > enemyRandNum)
+                        {
+                            playerScore++;
+                            Console.WriteLine("player wins this round!");
+                        }
+                        else if (enemyRandNum > playerRandNum)
+                        {
+                            enemyScore++;
+                            Console.WriteLine("enemy wins this round!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Draw!");
+                        }
+                        Console.WriteLine("players score (player: {0}, enemy: {1})\n", playerScore, enemyScore);
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (playerScore > enemyScore)
+            {
+                Console.WriteLine("\n\nPlayer Wins!!");
+            }
+            else if (playerScore < enemyScore)
+            {
+                Console.WriteLine("\n\nenemy Wins!!");
+            }
+            else
+            {
+                Console.WriteLine("\n\nDraw!");
+            }
+        }
+    }
+}
---
+++ Dice/README.md
@@ -0,0 +1,17 @@
+# Dice Rolling Game
+
+A simple console-based dice rolling game written in C#.
+
+![Game Screenshot](./assets/game_screenshot.png)
+
+## Overview
+
+This C# program implements a basic dice rolling game where the player competes against an AI enemy by rolling a six-sided die.
+The game provides a user-friendly interface and keeps track of the player's and enemy's scores.
+
+## Features
+
+- Press Enter to roll the dice and compete against the AI.
+- Exit the game by pressing 'Q.'
+- Interactive display of dice rolls and round outcomes.
+- Final display of the winner or a draw.
